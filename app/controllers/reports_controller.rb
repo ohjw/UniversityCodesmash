4499605593c6afbe814b897db6698afdfe5151ef
vagrant <vagrant@shopkeep-dev>
2016-04-06 15:48:04 +0000

    Olivia

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,9 +1,26 @@
 class ReportsController < WebServiceController
 
   def total_sales
+     totalSales = { number_of_sales: Sale.count, total: Sale.sum(:total) }
+     render json: totalSales
   end
 
   def sales_per_item
+      sales = Sale.all
+      burgerTotal = 0
+      chipTotal = 0
+      drinkTotal = 0
+
+      salesPerItem = {'burger' => {'number_of_sales' => "", 'total' => ""}, 'chips' => {'number_of_sales' => "", 'total' => ""}, 'soft drink' => {'number_of_sales' => "", 'total' => ""} }
+     
+      sales.each do |sale|
+	sale.items.each do |item|
+	   item['name'].equal? 'burger'
+	     burgerTotal += item['burger']['total']	
+	end
+      end     
+      render json: salesPerItem
+	
   end
 
 end
--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,11 +1,12 @@
 class SalesController < WebServiceController
 
+
   def index # Show all the Sales
     render json: Sale.all
   end
 
   def show # Show a single Sale by 'id'
-    id = nil # Hint: look at the 'params' hash
+    id = params['id']
     sale = Sale.find id
     render json: sale
   end
@@ -13,8 +14,17 @@ class SalesController < WebServiceController
   def create
     # Hint: check if your newly created sale is valid
     #       by checking sale.valid? and looking at the
-    #       sale.errors array in the debugger
-    sale = Sale.create sale_params
+    #       sale.errors array in the debugger 
+    total = 0 
+    sale_params['items'].each do |item|
+	total += item['price'].to_f
+	if item.has_key? 'discount'
+	   total -= item['discount']['amount'].to_f
+	end	
+    end
+    sale_params['total'] = total
+    sale = Sale.new sale_params
+    sale.save
     render json: sale
   end
 
